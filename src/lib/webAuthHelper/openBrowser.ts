eb9a3f37746e4997d28e75c69616bc364b9d60ee
German Swan <glebedev@code.store>
2020-06-29 20:18:56 +0300

    fix auth link

--- src/lib/webAuthHelper/openBrowser.ts
+++ src/lib/webAuthHelper/openBrowser.ts
@@ -1,6 +1,6 @@
 import open from 'open';
 import { ChildProcess } from 'child_process';
 
-const authLink = 'https://api.codestore.dev/authentication-service/authorize?cli=true';
+const authLink = `${process.env.CODESTORE_GATEWAY_HOST || 'https://api.code.store'}/authentication-service/authorize?cli=true`;
 
 export default (): Promise<ChildProcess> => open(authLink);
