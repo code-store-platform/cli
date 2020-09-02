2ae7029f7bfc8bb6c57708c4a75a8f2e86cc8206
dmukhopadov <d.mukhopadov@futuro.team>
2020-09-02 16:50:03 +0300

    refactoring, fixed issues

--- src/lib/service-worker.ts
+++ src/lib/service-worker.ts
@@ -1,18 +1,18 @@
+import { join } from 'path';
 import { parse } from 'yaml';
 import { yellow } from 'chalk';
-import { PromisifiedFs } from 'codestore-utils';
+import { PromisifiedFs, PathsResolverTool } from 'codestore-utils';
 import IServiceConfig from '../interfaces/service-config.interface';
-import Paths, { join } from '../common/constants/paths';
 import { WrongFolderError } from './errors';
 
 export default class ServiceWorker {
   private configFiles = {
     codestore: {
-      path: join(Paths.ROOT, 'codestore.yaml'),
+      path: join(PathsResolverTool.ROOT, 'codestore.yaml'),
       error: new WrongFolderError('You must be in code.store service root folder to invoke this command.\nCheck if codestore.yaml and schema.graphql exist.'),
     },
     schema: {
-      path: join(Paths.SRC, 'schema.graphql'),
+      path: PathsResolverTool.SCHEMA,
       error: new WrongFolderError(`Cannot find schema.graphql, make sure that you invoke this command from the service root folder or use ${yellow('cs service:pull')} to restore your graphql.schema.`),
     },
   };
@@ -36,29 +36,5 @@ export default class ServiceWorker {
     const file = await PromisifiedFs.readFile(configPath);
 
     return parse(file.toString()) as IServiceConfig;
-  }
-
-  public async loadEntitiesAndMutationsPaths(): Promise<string[]> {
-    const entities = join(process.cwd(), 'src', 'data', 'entities');
-    const migrations = join(process.cwd(), 'src', 'data', 'migrations');
-
-    const entitiesPaths = await this.loadFilePaths(entities);
-    const migrationsPaths = await this.loadFilePaths(migrations);
-
-    return [
-      ...entitiesPaths, ...migrationsPaths,
-    ];
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  private async loadFilePaths(path: string): Promise<string[]> {
-    try {
-      return await PromisifiedFs.readdir(path).then((data) => data
-        .filter((file) => /.ts$/.test(file))
-        .map((file) => join(path, file)));
-    } catch (e) {
-      if (e.code !== 'ENOENT') throw e;
-      return [];
-    }
   }
 }
